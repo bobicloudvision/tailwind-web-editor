c6ea0003a782226948714d4457d5547fce76c334
Bozhidar Slaveykov <bobi@microweber.com>
2023-02-25 01:21:31 +0200

    update

--- resources/js/Pages/Editor/Page.vue
+++ resources/js/Pages/Editor/Page.vue
@@ -29,7 +29,15 @@ export default {
             let editorIframeDocument = editorIframe.contentDocument;
             let editorIframeBody = editorIframeDocument.body;
 
-            // Set all contenteditable elements to true
+            var head  = editorIframeDocument.getElementsByTagName('head')[0];
+            var link  = editorIframeDocument.createElement('link');
+            link.id   = 'js-tailwind-editor-iframe-stylesheet';
+            link.rel  = 'stylesheet';
+            link.type = 'text/css';
+            link.href = 'live-edit/elements.css';
+            link.media = 'all';
+            head.appendChild(link);
+
             let contentEditableElementsTags = [
                 'h1',
                 'h2',
@@ -40,11 +48,65 @@ export default {
                 'p',
                 'span',
                 'b',
+                'button',
             ];
+
+
+            function removeAllContentEditableElements() {
+                for (var i = 0; i < contentEditableElementsTags.length; i++) {
+                    let editorTag = editorIframeBody.getElementsByTagName(contentEditableElementsTags[i]);
+                    for (var j = 0; j < editorTag.length; j++) {
+                        editorTag[j].removeAttribute('contenteditable', 'true');
+                        editorTag[j].classList.remove('js-live-edit-element-border');
+                        editorTag[j].classList.remove('js-live-edit-element-background');
+                    }
+                }
+            }
+
             for (var i = 0; i < contentEditableElementsTags.length; i++) {
                 let editorTag = editorIframeBody.getElementsByTagName(contentEditableElementsTags[i]);
                 for (var j = 0; j < editorTag.length; j++) {
-                    editorTag[j].setAttribute('contenteditable', 'true');
+
+                    editorTag[j].classList.add('js-live-edit-element');
+
+                    editorTag[j].addEventListener('keydown', function () {
+
+                    });
+
+                    editorTag[j].addEventListener('click', function () {
+                        if (!this.hasAttribute('contenteditable')) {
+                            removeAllContentEditableElements();
+                            this.classList.add('js-live-edit-element-border');
+                            this.classList.add('js-live-edit-element-background');
+                        }
+                    });
+
+                    editorTag[j].addEventListener('mouseover', function () {
+                        if (!this.hasAttribute('contenteditable')) {
+                            this.classList.add('js-live-edit-element-border');
+                        }
+                    });
+
+                    editorTag[j].addEventListener('dblclick', function () {
+                        removeAllContentEditableElements();
+                        this.setAttribute('contenteditable', 'true');
+                    });
+
+                    editorTag[j].addEventListener('mouseenter', function () {
+
+                    });
+
+                    editorTag[j].addEventListener('mouseleave', function () {
+                        let removeClasses = true;
+                        if (this.hasAttribute('contenteditable')) {
+                            removeClasses = false;
+                        }
+                        if (removeClasses) {
+                            this.classList.remove('js-live-edit-element-border');
+                            this.classList.remove('js-live-edit-element-background');
+                        }
+                    });
+
                 }
             }
 
