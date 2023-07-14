27238b5c57c2b9b6bd4ef5a18124d065c7da73f8
Bozhidar Slaveykov <bobicloudvision@gmail.com>
2023-07-14 13:28:35 +0300

    update

--- packages/cloudvision-apps/webesembly-editor/webpack.mix.js
+++
@@ -1,6 +0,0 @@
-/*npx mix watch*/
-
-let mix = require('laravel-mix');
-mix.sass('src/resources/scss/editor-iframe/webesembly-iframe.scss', 'dist/webesembly');
-mix.ts('src/resources/js/webesembly-editor.js', 'dist/webesembly')
-    .vue({ version: 3 });
